87fe5dbfd9573857dd7d5f0cd5080f9336f71a40
Tim Zimmermann <tim@linux4.de>
2023-04-10 08:26:37 +0200

    Settings: Add settings to change QS column count [2/2]
    Change-Id: I01ee97fb58f9cf1a0b74f44c1494d68571660ba4

--- src/org/leafos/settings/qs/QSPanelSettings.java
+++ src/org/leafos/settings/qs/QSPanelSettings.java
@@ -16,6 +16,12 @@
 
 package org.leafos.settings.qs;
 
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -25,6 +31,28 @@ import com.android.settingslib.search.SearchIndexable;
 public class QSPanelSettings extends DashboardFragment {
 
     private static final String TAG = "QSPanelSettings";
+    private static final String[] qsCustPreferences = { "qs_tile_shape",
+            "qqs_num_columns", "qqs_num_columns_landscape",
+            "qs_num_columns", "qs_num_columns_landscape" };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+        boolean qsStyleRound = Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                Settings.Secure.QS_STYLE_ROUND, 1, UserHandle.USER_CURRENT) == 1;
+
+        if (!qsStyleRound) {
+            for (String key : qsCustPreferences) {
+                Preference preference = preferenceScreen.findPreference(key);
+                if (preference != null) {
+                    preference.setEnabled(false);
+                }
+            }
+        }
+    }
 
     @Override
     public int getMetricsCategory() {
