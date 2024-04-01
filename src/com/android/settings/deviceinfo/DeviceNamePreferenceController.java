a1d1f754e88baf852fc4949a5e961df4ce7b533d
afterallafk <shivamatiet2001@gmail.com>
2024-04-01 16:25:53 +0000

    Settings: Fix NPE for About Phone crash
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
+++ src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
@@ -74,13 +74,16 @@ public class DeviceNamePreferenceController extends BasePreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        mDeviceCard = mPreference.findViewById(R.id.about_device_header);
-        final CharSequence deviceName = getSummary();
-	mDeviceCard.setDeviceName(deviceName.toString(), mWifiDeviceNameTextValidator.isTextValid(deviceName.toString()));
-        mDeviceCard.setListener(s -> {
+        mPreference = (LayoutPreference) screen.findPreference(getPreferenceKey());
+        if (mPreference != null) {
+            mDeviceCard = mPreference.findViewById(R.id.about_device_header);
+            final CharSequence deviceName = getSummary();
+            mDeviceCard.setDeviceName(deviceName.toString(), mWifiDeviceNameTextValidator.isTextValid(deviceName.toString()));
+            mDeviceCard.setListener(s -> {
             setDeviceName(s);
             return Unit.INSTANCE;
         });
+      }
     }
 
     private void initializeDeviceName() {
